fbc2f372c084ff37cdc488e26f7306777e2c07e7
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-14 12:48:11 -0500

    Agregar clase PrincipalStack para probar la implementación de TadPila con un ejemplo de pila de enteros. Modificar el método apilar en TadPila para mejorar la claridad del código.

---
+++ src/LaboratorioPruebas/PrincipalStack.java
@@ -0,0 +1,10 @@
+package LaboratorioPruebas;
+
+import Stack.*; // importa todo el package de Stack
+
+public class PrincipalStack {
+    public static void main(String[] args) {
+        TadPila<Integer> pilaInteger = new TadPila<>("Pila De Integer");
+    }
+
+}
